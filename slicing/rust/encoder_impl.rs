6f8e99c44ece87e02ffe3552cb0112ba9f708cec
PaulGD03 <paulskapczyk@gmail.com>
2026-03-20 16:34:19 +0100

    Reduce minimum anti-aliasing alpha percentage from 50% to 30% in encoder configuration

--- slicing/rust/encoder_impl.rs
+++ slicing/rust/encoder_impl.rs
@@ -468,7 +468,7 @@ mod tests {
             container_compression_level: 2,
             anti_aliasing_level: "Off".to_string(),
             aa_on_supports: false,
-            minimum_aa_alpha_percent: 50.0,
+            minimum_aa_alpha_percent: 30.0,
             mirror_x: false,
             mirror_y: false,
             triangles_xyz: vec![],
