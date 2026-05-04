e11caa1db0856119fe5eccec25cca39761f4d35d
PaulGD03 <paulskapczyk@gmail.com>
2026-05-04 11:53:04 +0200

    fix: Update homepage URL in CTB plugin manifest

--- pluginManifest.ts
+++ pluginManifest.ts
@@ -4,6 +4,7 @@ export const CTB_PLUGIN_MANIFEST = {
   name: 'CTB Plugin',
   version: '0.1.0',
   description: 'CTB format support plugin for DragonFruit.',
+  homepage: 'https://github.com/Open-Resin-Alliance/df-plugin-ctb',
   printerPresets: [],
   materialTemplates: [],
 };
