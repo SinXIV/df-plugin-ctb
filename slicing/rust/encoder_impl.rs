f189d940f13c0d2c7cd509609554fee3a9021b61
PaulGD03 <paulskapczyk@gmail.com>
2026-05-15 23:44:45 +0200

    feat: Add z_blend_auto_fade and z_blend_minimum_alpha_percent parameters to encoder settings

--- slicing/rust/encoder_impl.rs
+++ slicing/rust/encoder_impl.rs
@@ -911,6 +911,8 @@ mod tests {
             mirror_y: false,
             z_blend_look_back: 2,
             z_blend_fade_px: 20,
+            z_blend_auto_fade: true,
+            z_blend_minimum_alpha_percent: 0.0,
             z_blend_debug_color_overlay: false,
             triangles_xyz: vec![],
             metadata_json: "{}".to_string(),
